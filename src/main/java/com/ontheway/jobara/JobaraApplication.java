f53652d9654deddbbaa60cb1fa6ee365da024fca
BoyunC <cbyn0819@gmail.com>
2022-10-24 07:29:09 +0900

    update README.md

---
+++ src/main/java/com/ontheway/jobara/JobaraApplication.java
@@ -0,0 +1,13 @@
+package com.ontheway.jobara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ontheway/jobara/JobaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontheway.jobara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
